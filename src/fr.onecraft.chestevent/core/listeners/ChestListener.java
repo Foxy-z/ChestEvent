a57d69cb9b7bffcf9d18d4d069eb97399973594b
TheFoxy41 <thefoxy41@gmail.com>
2018-07-19 21:42:04 +0200

    TheFoxy41

--- src/fr.onecraft.chestevent/core/listeners/ChestListener.java
+++ src/fr.onecraft.chestevent/core/listeners/ChestListener.java
@@ -3,7 +3,6 @@ package fr.onecraft.chestevent.core.listeners;
 import fr.onecraft.chestevent.ChestEvent;
 import fr.onecraft.chestevent.core.objects.Chest;
 import fr.onecraft.chestevent.core.objects.Menu;
-import fr.onecraft.chestevent.core.objects.Model;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -26,22 +25,23 @@ public class ChestListener implements Listener {
 
 
     /*
-     * Détection du clic sur un item
+     * When an item is clicked
      */
     @EventHandler
     public void onInterractWithChest(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         Action action = event.getAction();
+        // if it is a right click
         if (!action.equals(Action.RIGHT_CLICK_BLOCK) && !action.equals(Action.RIGHT_CLICK_AIR))
             return;
+
         ItemStack item = player.getItemInHand();
+        // if clicked item is a chest
         if (item == null || !item.getType().equals(Material.CHEST))
             return;
         ItemMeta meta = item.getItemMeta();
 
-        /*
-         * Vérification du nom de l'item coffre
-         */
+        // check if the chest's name is right
         if (meta.getDisplayName() == null || !meta.getDisplayName().startsWith("§6§lCoffre d'événement §f§ln°"))
             return;
 
@@ -53,6 +53,8 @@ public class ChestListener implements Listener {
         }
 
         Chest chest = Chest.fromId(plugin, id);
+
+        // delete the chest if he is null
         if (chest == null) {
             player.getInventory().setItemInHand(new ItemStack(Material.AIR));
             player.updateInventory();
@@ -61,6 +63,7 @@ public class ChestListener implements Listener {
             return;
         }
 
+        // open the inventory if the player has the permission
         if (player.hasPermission(chest.getPermission())) {
             Menu menu = chest.getMenu();
             player.openInventory(menu.getPage(1));
@@ -69,41 +72,46 @@ public class ChestListener implements Listener {
     }
 
     /*
-     * Lorsqu'item du menu est cliqué
+     * When an menu's item is clicked
      */
     @EventHandler
     public void onUseButton(InventoryClickEvent event) {
+        // if this is not an inventory from the plugin
         if (!(event.getInventory().getHolder() instanceof Menu)) return;
         Player player = (Player) event.getWhoClicked();
         Inventory inventory = event.getInventory();
 
         Menu menu = (Menu) inventory.getHolder();
         ItemStack clickedItem = event.getCurrentItem();
+        // if clicked item is a button
         if (clickedItem != null && (clickedItem.getType().equals(Menu.PAGE_BUTTON) || clickedItem.getType().equals(Menu.SEPARATION_BUTTON))) {
+            // if clicked button is next page button
             if (clickedItem.equals(inventory.getItem(51))) {
                 if (menu.getCurrentPage() < Math.ceil((double) menu.getSize() / 45)) {
                     menu.setCurrentPage(menu.getCurrentPage() + 1);
                     player.openInventory(menu.getPage(menu.getCurrentPage()));
                 }
                 event.setCancelled(true);
+                // if clicked button is previous page button
             } else if (clickedItem.equals(inventory.getItem(47))) {
                 if (menu.getCurrentPage() > 1) {
                     menu.setCurrentPage(menu.getCurrentPage() - 1);
                     player.openInventory(menu.getPage(menu.getCurrentPage()));
                 }
                 event.setCancelled(true);
-            } else if (clickedItem.equals(inventory.getItem(49)))
+                // if clicked button is middle button (useless)
+            } else if (clickedItem.equals(inventory.getItem(49))) {
                 event.setCancelled(true);
+            }
         }
     }
 
     /*
-     * Lorsqu'un item du menu est cliqué
+     * When a menu's item is clicked
      */
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked().getOpenInventory().getTopInventory().getHolder() instanceof Menu)
-            event.setCancelled(true);
+        // if this is not an inventory from the plugin
         if (!(event.getInventory().getHolder() instanceof Menu)) return;
         Player player = (Player) event.getWhoClicked();
         Inventory inventory = event.getClickedInventory();
@@ -112,30 +120,25 @@ public class ChestListener implements Listener {
         Menu menu = (Menu) inventory.getHolder();
         event.setCancelled(true);
         ItemStack itemStack = event.getCurrentItem();
+        // check if the clicked item is not null or not air
         if (itemStack == null || itemStack.getType().equals(Material.AIR)) return;
 
-        if (menu.getSize() > 54) {
-            if (itemStack.equals(inventory.getItem(51)) || itemStack.equals(inventory.getItem(49)) || itemStack.equals(inventory.getItem(47)))
+        // if player's inventory is not full
+        if (player.getInventory().firstEmpty() != -1) {
+            // check if clicked item is not a button
+            if (menu.getSize() > 54 && event.getRawSlot() > 45) {
                 return;
-            if (player.getInventory().firstEmpty() != -1) {
-                menu.getItems().remove(itemStack);
-                player.getInventory().addItem(itemStack);
-                inventory.setItem(event.getRawSlot(), new ItemStack(Material.AIR));
-                player.updateInventory();
-            } else {
-                player.sendMessage(ChestEvent.ERROR + "Votre inventaire est plein !");
             }
+            // give the item to the player and remove it from the chest
+            menu.getItems().remove(itemStack);
+            player.getInventory().addItem(itemStack);
+            inventory.setItem(event.getRawSlot(), new ItemStack(Material.AIR));
+            player.updateInventory();
         } else {
-            if (player.getInventory().firstEmpty() != -1) {
-                menu.getItems().remove(itemStack);
-                player.getInventory().addItem(itemStack);
-                inventory.setItem(event.getRawSlot(), new ItemStack(Material.AIR));
-                player.updateInventory();
-            } else {
-                player.sendMessage(ChestEvent.ERROR + "Votre inventaire est plein !");
-            }
+            player.sendMessage(ChestEvent.ERROR + "Votre inventaire est plein !");
         }
 
+        // delete the inventory if it is empty
         if (menu.getItems().isEmpty()) {
             player.getInventory().setItemInHand(new ItemStack(Material.AIR));
             player.updateInventory();
@@ -145,10 +148,11 @@ public class ChestListener implements Listener {
     }
 
     /*
-     * Quand un inventaire est fermé
+     * When an inventory is closed
      */
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
+        // if this is not an inventory from the plugin
         if (!(event.getInventory().getHolder() instanceof Menu))
             return;
 
